b95aeec404300805e29c365110310eff416d71e1
Suvendu_Behera <suvendu-78>
2026-09-18 11:31:55 +0530

    update

--- DataBase/databse.js
+++ DataBase/databse.js
@@ -12,57 +12,20 @@ import Clinic from "../Const.js";
 //   }
 // };
 // export default Database;
-// const Database = async () => {
-//   try {
-//     if (mongoose.connection.readyState === 1) {
-//       return mongoose.connection;
-//     }
-
-//     const data = await mongoose.connect(`${process.env.MONGODB_URL}/${Clinic}`);
-
-//     console.log("database connected successfully");
-
-//     return data;
-//   } catch (error) {
-//     console.log("database not connected:", error.message);
-//     throw error;
-//   }
-// };
-// export default Database;
-
-import mongoose from "mongoose";
-import Clinic from "../Const.js";
-
-let dbPromise = null;
-
 const Database = async () => {
-  if (mongoose.connection.readyState === 1) {
-    return mongoose.connection;
-  }
-
-  if (!dbPromise) {
-    const mongoUrl = process.env.MONGODB_URL;
-
-    if (!mongoUrl) {
-      throw new Error("MONGODB_URL is not defined");
+  try {
+    if (mongoose.connection.readyState === 1) {
+      return mongoose.connection;
     }
 
-    dbPromise = mongoose
-      .connect(`${mongoUrl}/${Clinic}`, {
-        serverSelectionTimeoutMS: 10000,
-      })
-      .then(() => {
-        console.log("MongoDB connected successfully");
-        return mongoose.connection;
-      })
-      .catch((error) => {
-        dbPromise = null;
-        console.error("MongoDB connection error:", error.message);
-        throw error;
-      });
+    const data = await mongoose.connect(`${process.env.MONGODB_URL}/${Clinic}`);
+
+    console.log("database connected successfully");
+
+    return data;
+  } catch (error) {
+    console.log("database not connected:", error.message);
+    throw error;
   }
-
-  return dbPromise;
 };
-
 export default Database;
