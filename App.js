960536e17c98e57931657d6032c2793fdd8d22c3
Suvendu_Behera <suvendu-78>
2026-09-20 11:32:17 +0530

    update

--- App.js
+++ App.js
@@ -1,38 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-
-dotenv.config();
-
-const App = express();
-
-App.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
-  }),
-);
-
-App.use(express.json({ limit: "50mb" }));
-App.use(express.urlencoded({ extended: true }));
-App.use(cookieParser());
-
-import Router from "./Router/router.js";
-import AdminRouter from "./Admin/Admin_Router/admin_Router.js";
-import PattnerRouter from "./Admin/Admin_Router/pattner_Router.js";
-
-App.use("/api/v1/user", Router);
-App.use("/api/v1/onboard", AdminRouter);
-App.use("/api/v1/pattner", PattnerRouter);
-
-export default App;
-
 // import express from "express";
 // import dotenv from "dotenv";
 // import cors from "cors";
 // import cookieParser from "cookie-parser";
-// import Database from "./DataBase/databse.js";
+
 // dotenv.config();
 
 // const App = express();
@@ -44,44 +14,74 @@ export default App;
 //   }),
 // );
 
-// App.use(async (req, res, next) => {
-//   try {
-//     await Database();
-//     next();
-//   } catch (error) {
-//     console.log("DATABASE CONNECTION ERROR:", error.message);
-
-//     return res.status(500).json({
-//       success: false,
-//       message: "Database connection failed",
-//     });
-//   }
-// });
-// App.get("/", (req, res) => {
-//   res.json({
-//     success: true,
-//     message: "Physio backend is running",
-//   });
-// });
-
 // App.use(express.json({ limit: "50mb" }));
 // App.use(express.urlencoded({ extended: true }));
 // App.use(cookieParser());
 
-// App.get("/", (req, res) => {
-//   res.json({
-//     success: true,
-//     message: "Physio backend is running",
-//   });
-// });
-
 // import Router from "./Router/router.js";
-// App.use("/api/v1/user", Router);
-
 // import AdminRouter from "./Admin/Admin_Router/admin_Router.js";
-// App.use("/api/v1/onboard", AdminRouter);
-
 // import PattnerRouter from "./Admin/Admin_Router/pattner_Router.js";
+
+// App.use("/api/v1/user", Router);
+// App.use("/api/v1/onboard", AdminRouter);
 // App.use("/api/v1/pattner", PattnerRouter);
 
 // export default App;
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import Database from "./DataBase/databse.js";
+dotenv.config();
+
+const App = express();
+
+App.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  }),
+);
+
+App.use(async (req, res, next) => {
+  try {
+    await Database();
+    next();
+  } catch (error) {
+    console.log("DATABASE CONNECTION ERROR:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Database connection failed",
+    });
+  }
+});
+App.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Physio backend is running",
+  });
+});
+
+App.use(express.json({ limit: "50mb" }));
+App.use(express.urlencoded({ extended: true }));
+App.use(cookieParser());
+
+App.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Physio backend is running",
+  });
+});
+
+import Router from "./Router/router.js";
+App.use("/api/v1/user", Router);
+
+import AdminRouter from "./Admin/Admin_Router/admin_Router.js";
+App.use("/api/v1/onboard", AdminRouter);
+
+import PattnerRouter from "./Admin/Admin_Router/pattner_Router.js";
+App.use("/api/v1/pattner", PattnerRouter);
+
+export default App;
